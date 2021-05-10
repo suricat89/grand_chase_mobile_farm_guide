fc6e0cb5f2a0f8776c4fb5adb9b47c76232f0186
Thiago Monteiro de Paula <thiago.paula@luizalabs.com>
2021-05-10 19:07:22 -0300

    fix(login): layout crash when keyboard appears

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -17,6 +17,7 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Container(
